02c449f6a811ae049fc6cb980b2cfc73f90dce92
pratik1803 <pratikvaidya1803@gmail.com>
2022-09-03 00:38:50 +0530

    fixed es-lint error

--- pages/password-recovery/index.tsx
+++ pages/password-recovery/index.tsx
@@ -5,7 +5,6 @@ import Entrance from "../../components/Entrance/Entrance";
 import Styles from "../../styles/login/login.module.scss";
 import { sendUserRecoveryEmail } from "../../typings/services/user/sendUserRecoveryEmail.service";
 
-
 function PasswordRecovery() {
 	const [loading, setLoading] = useState(false);
 	const [enteredDetails, setUserDetails] = useState({
@@ -13,77 +12,78 @@ function PasswordRecovery() {
 		email: "",
 	});
 
-	async function sendUserRecoveryEmailTrigger(){
+	async function sendUserRecoveryEmailTrigger() {
 		setLoading(true);
 		try {
-			if(enteredDetails.username==="" && enteredDetails.email==="")return alert("Please provide username or email first!");
+			if (enteredDetails.username === "" && enteredDetails.email === "")
+				return alert("Please provide username or email first!");
 			const result = await sendUserRecoveryEmail(enteredDetails);
-			if(!result.data.success) throw new Error("Something went wrong!");
+			if (!result.data.success) throw new Error("Something went wrong!");
 			alert("Recovery email sent successfully!");
 		} catch (error) {
 			console.log(`${error}`);
-		};
+		}
 		setLoading(false);
-	};
+	}
 
 	return (
 		<Entrance>
 			<div className={Styles.login}>
-				
-						<h3>Forgot your password?</h3>
-						<h3>Don't worry, we've got you covered!</h3>
-						<form
-							onSubmit={(e) => {
-								e.preventDefault();
-								sendUserRecoveryEmailTrigger();
+				<h3>Forgot your password?</h3>
+				{/* eslint-disable-next-line react/no-unescaped-entities */}
+				<h3>Don't worry, we've got you covered!</h3>
+				<form
+					onSubmit={(e) => {
+						e.preventDefault();
+						sendUserRecoveryEmailTrigger();
+					}}
+				>
+					<Stack spacing={2} direction={"column"}>
+						<TextField
+							autoFocus={true}
+							size="small"
+							label="Username"
+							type={"text"}
+							value={enteredDetails.username}
+							onChange={(e) => {
+								setUserDetails((prev) => ({
+									...prev,
+									username: e.target.value,
+								}));
 							}}
-						>
-							<Stack spacing={2} direction={"column"}>
-								<TextField
-									autoFocus={true}
-									size="small"
-									label="Username"
-									type={"text"}
-									value={enteredDetails.username}
-									onChange={(e) => {
-										setUserDetails((prev) => ({
-											...prev,
-											username: e.target.value,
-										}));
-									}}
-								/>
-								<h4>
-									<u>OR</u>
-								</h4>
-								<TextField
-									size="small"
-									label="Email"
-									type={"email"}
-									value={enteredDetails.email}
-									onChange={(e) => {
-										setUserDetails((prev) => ({
-											...prev,
-											email: e.target.value,
-										}));
-									}}
-								/>
-								<div className={Styles.login_btn}>
-									<Button type="submit">
-										{loading ? (
-											<CircularProgress
-												style={{
-													color: "#f9f9f9",
-													width: "24px",
-													height: "24px",
-												}}
-											/>
-										) : (
-											"Send Recovery Email!"
-										)}
-									</Button>
-								</div>
-							</Stack>
-						</form>
+						/>
+						<h4>
+							<u>OR</u>
+						</h4>
+						<TextField
+							size="small"
+							label="Email"
+							type={"email"}
+							value={enteredDetails.email}
+							onChange={(e) => {
+								setUserDetails((prev) => ({
+									...prev,
+									email: e.target.value,
+								}));
+							}}
+						/>
+						<div className={Styles.login_btn}>
+							<Button type="submit">
+								{loading ? (
+									<CircularProgress
+										style={{
+											color: "#f9f9f9",
+											width: "24px",
+											height: "24px",
+										}}
+									/>
+								) : (
+									"Send Recovery Email!"
+								)}
+							</Button>
+						</div>
+					</Stack>
+				</form>
 			</div>
 		</Entrance>
 	);
--- pages/password-recovery/recovery/index.tsx
+++ pages/password-recovery/recovery/index.tsx
@@ -6,7 +6,6 @@ import Styles from "../../../styles/login/login.module.scss";
 import { useRouter } from "next/router";
 import { changePassword } from "../../../typings/services/user/changePassword.service";
 
-
 function PasswordRecovery() {
 	const [loading, setLoading] = useState(false);
 	const router = useRouter();
@@ -16,74 +15,82 @@ function PasswordRecovery() {
 		confirmPassword: "",
 	});
 
-	async function updateInfo(){
+	async function updateInfo() {
 		setLoading(true);
 		try {
-			if(!router.query.uid)return;
-			if(newPassword.password !== newPassword.confirmPassword){
+			if (!router.query.uid) return;
+			if (newPassword.password !== newPassword.confirmPassword) {
 				setLoading(false);
 				return alert("Passwords don't match!");
-			};
-			const result = await changePassword({uid:router.query.uid as string, updatedPass:newPassword.password});
-			if(result.data.success)throw new Error("Something went wrong while updating the password!");
-			setLoading(false);	
+			}
+			const result = await changePassword({
+				uid: router.query.uid as string,
+				updatedPass: newPassword.password,
+			});
+			if (result.data.success)
+				throw new Error(
+					"Something went wrong while updating the password!"
+				);
+			setLoading(false);
 			alert("Password Updated Successfully!");
 		} catch (error) {
 			console.log(`Err in updating the password: ${error}`);
-		};
+		}
 		setLoading(false);
 	}
 
 	return (
 		<Entrance>
 			<div className={Styles.login}>
-            <h3>Enter your new password:</h3>
-						<form onSubmit={(e)=>{
-							e.preventDefault();
-							updateInfo();
-						}}>
-							<Stack spacing={2} direction={"column"}>
-								<TextField
-									type={"password"}
-									label={"Enter new password"}
-									value={newPassword.password}
-									size={"small"}
-									onChange={(e) => {
-										setNewPassword((prev) => ({
-											...prev,
-											password: e.target.value,
-										}));
-									}}
-								/>
-								<TextField
-									type={"password"}
-									label={"Confirm new password"}
-									size={"small"}
-									value={newPassword.confirmPassword}
-									onChange={(e) => {
-										setNewPassword((prev) => ({
-											...prev,
-											confirmPassword: e.target.value,
-										}));
-									}}
-								/>
-								<div className={Styles.login_btn}>
-									<Button type="submit">
-										{loading ? (
-											<CircularProgress
-												style={{
-													color: "#f9f9f9",
-													width: "24px",
-													height: "24px",
-												}}
-											/>
-										) : (
-											"Update Password"
-										)}
-									</Button>
-								</div>
-							</Stack>
-						</form>
+				<h3>Enter your new password:</h3>
+				<form
+					onSubmit={(e) => {
+						e.preventDefault();
+						updateInfo();
+					}}
+				>
+					<Stack spacing={2} direction={"column"}>
+						<TextField
+							type={"password"}
+							label={"Enter new password"}
+							value={newPassword.password}
+							size={"small"}
+							onChange={(e) => {
+								setNewPassword((prev) => ({
+									...prev,
+									password: e.target.value,
+								}));
+							}}
+						/>
+						<TextField
+							type={"password"}
+							label={"Confirm new password"}
+							size={"small"}
+							value={newPassword.confirmPassword}
+							onChange={(e) => {
+								setNewPassword((prev) => ({
+									...prev,
+									confirmPassword: e.target.value,
+								}));
+							}}
+						/>
+						<div className={Styles.login_btn}>
+							<Button type="submit">
+								{loading ? (
+									<CircularProgress
+										style={{
+											color: "#f9f9f9",
+											width: "24px",
+											height: "24px",
+										}}
+									/>
+								) : (
+									"Update Password"
+								)}
+							</Button>
+						</div>
+					</Stack>
+				</form>
 			</div>
 		</Entrance>
 	);
